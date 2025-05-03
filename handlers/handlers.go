087c161980232b6ecd193d67016be42d1a105ad0
emperorsixpacks <andrewdvd10@gmail.com>
2025-05-03 14:02:37 +0100

    feat: get todos stable

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -11,6 +11,7 @@ import (
 var DB = databse.GetCache()
 
 type Todo struct {
+	Id    int    `json:"page-id"`
 	Title        string `json:"title"`
 	Summary      string `json:"summary"`
 	Is_completed bool   `json:"is-completed"`
@@ -21,10 +22,16 @@ type Page struct {
 	Todos []Todo `json:"todos"`
 }
 
+type Message struct {
+	ErrorMessage string `json:"message"`
+	StatusCode   int    `json:"status"`
+}
+
 func GetTodos(ctx *fiber.Ctx) error {
 	item, ok := DB.Get("todos")
 	if !ok {
-		return ctx.Status(fiber.StatusNotFound).SendString("Not todos found at the momenet, try creating some now ")
+		message := Message{"No todos found now", fiber.StatusNotFound}
+		return ctx.Status(message.StatusCode).JSON(message)
 	}
 	item_json, err := json.Marshal(item)
 	if err != nil {
@@ -35,7 +42,8 @@ func GetTodos(ctx *fiber.Ctx) error {
 }
 
 func GetTodo(ctx *fiber.Ctx) error {
-	return nil
+	id := ctx.Params("id")
+
 }
 
 func DeleteTodo(ctx *fiber.Ctx) error {
