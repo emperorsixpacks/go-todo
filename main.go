4940c1378ff85041cf30333ffe442fc3c6156d58
emperorsixpacks <andrewdvd10@gmail.com>
2025-05-03 15:23:12 +0100

    feat: added initdb funcion

--- main.go
+++ main.go
@@ -3,21 +3,32 @@ package main
 import (
 	"log"
 
+	database "github.com/emperorsixpacks/go-todo/database"
 	"github.com/emperorsixpacks/go-todo/handlers"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
-  setUpRoutes(app)
+	initDB()
+	setUpRoutes(app)
 	log.Printf("Starting service on port :3000")
 	app.Listen(":3000")
 }
 
 func setUpRoutes(a *fiber.App) {
-	a.Get("/todos", handlers.GetTodos)
-	a.Get("/todos/:id", handlers.GetTodo)
-	a.Get("/delete/:id", handlers.DeleteTodo)
-	a.Get("/update/:id", handlers.UpdateTodo)
-	a.Get("/create", handlers.CreateTodo)
+	a.Get("/todos", handlers.GetTasks)
+	a.Get("/todos/:id", handlers.GetTask)
+	a.Delete("/delete/:id", handlers.DeleteTask)
+	a.Put("/update/:id", handlers.UpdateTask)
+	a.Post("/create", handlers.CreateTask)
+}
+
+func initDB() {
+	_tasksList := database.TasksList{Tasks: make([]*database.Task, 0)}
+
+	db := database.GetCache()
+	if _, ok := db.Get("todos"); !ok {
+		db.Set("todos", _tasksList, 0)
+	}
 }
