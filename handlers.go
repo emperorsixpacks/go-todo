9ea69e33515a45b78d37af99c2704ae10dec4b18
emperorsixpacks <andrewdvd10@gmail.com>
2025-05-03 03:24:15 +0100

    feat: added handlers

---
+++ handlers.go
@@ -0,0 +1,11 @@
+package todoapp
+
+import "github.com/gofiber/fiber/v2"
+
+func GetTodo(ctx *fiber.Ctx) {}
+
+func DeleteTodo(ctx *fiber.Ctx) {}
+
+func UpdateTodo(ctx *fiber.Ctx) {}
+
+func CreateTodo(ctx *fiber.Ctx) {}
